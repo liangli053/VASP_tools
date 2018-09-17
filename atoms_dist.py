81f9c8c67e6d91a1b65ca6d0c890568be406ff9b
liangli053 <lli14@binghamton.edu>
2018-09-17 18:19:44 -0500

    updated, added parser

--- atoms_dist.py
+++ atoms_dist.py
@@ -1,13 +1,19 @@
 import numpy as np
 
 
-def atoms_dist(a, b, latt_para):
+def atoms_dist(a, b, latt_mat):
     """ Return the distance between atoms a and b.
-    a, b : array or list
-        Two vectors of dimension (1 x 3)
 
-    latt_para : array
-        2D array with dimension (3 x 3)
+    Arguments:
+    -------------------
+    a, b : array or list, dim = (1, 3)
+        Coordinates of two atoms in the cell.
+
+    latt_mat : array, dim = (3, 3)
+        Matrix consisting of lacttice vectors a, b and c.
+
+    Returns:
+    -------------------
     rtype : float
     """
-    return np.linalg.norm(np.dot((a - b), latt_para), 2)
+    return np.linalg.norm(np.dot((a - b), latt_mat), 2)
--- find_neighbors.py
+++ find_neighbors.py
@@ -1,6 +1,7 @@
 import numpy as np
-import math
+from Parse_POSCAR import parse_POSCAR
 from atoms_dist import atoms_dist
+import math
 
 
 def find_neighbors(ctrAtom, cutoff, POSCAR="POSCAR"):
@@ -9,6 +10,8 @@ def find_neighbors(ctrAtom, cutoff, POSCAR="POSCAR"):
     Use VASP 5 format POSCAR or CONTCAR files. Periodic boundary
     conditions are taken into accont of.
 
+    Arguments:
+    -------------------
     ctrAtom : str
         Species and index of the central atom. e.g.: "O12", "Fe3".
 
@@ -17,25 +20,13 @@ def find_neighbors(ctrAtom, cutoff, POSCAR="POSCAR"):
         All atoms within cutoff radius of the center atom are counted.
 
     POSCAR : str
-        Input file. Must be VASP 5 format.
+        Input file name. Must be VASP 5 format.
 
-    atomNames : list[str]
-        Line 6 of POSCAR. Atomic species.
-
-    atomNums : list[int]
-        Line 7 of POSCAR. Atomic numbers
-
-    atomNum_Dict : dict['str': int]
-        zip atomNames and atomNums to form the dictionary.
-        Each key represents one atomic species.
-
-    atomCoor_Dict : dict['str': 2D array]
-        Each key represents one atomic species. Values are 2D arrays
-        of atomic coordinates. Dimension of 2D array is contingent to atomNums.
-
-    res: dict[ 'str': List[ dict{str : list[float]} ] ]
-        return value. Each key represents one atomic species. Each value is
-        a list of dictionary {"atom_species + index" : coordinates}.
+    Returns:
+    -------------------
+    res : dict {'str': dict{str : list[float]} }
+        Each key represents one atomic species. Each value is
+        a dict {"element + index" : coordinates}.
     """
 
     # center atom species and index
@@ -48,41 +39,22 @@ def find_neighbors(ctrAtom, cutoff, POSCAR="POSCAR"):
         ctrAtom_name = ctrAtom[0:2]
         ctrAtom_index = int(ctrAtom[2:])
 
-    fin = open(POSCAR, 'r')
-    poscar = fin.read().splitlines()
-    scaling_para = float(poscar[1])
-    abc = np.array([[float(i) for i in line.split()] for line in poscar[2:5]])
-    # lattice parameters in angstrom
-    latt_para = abc * scaling_para
-    # Lines 6 and 7 of POSCAR. atomic species and corresponding atoms numbers
-    atomNames = poscar[5].split()
-    atomNums = list(map(int, poscar[6].split()))
-    # combine atom names and numbers into a dict
-    atomNum_Dict = dict(zip(atomNames, atomNums))
-    # read in the coordinates of each species
-    atomCoor_Dict = dict.fromkeys(atomNum_Dict, [])
-    st_line = 8  # starting line number of atom coordinates
-    for i in atomCoor_Dict.keys():
-        end_line = st_line + atomNum_Dict[i]
-        coor = np.array([[float(e) for e in line.split()[0:3]] for line in poscar[st_line: end_line]])
-        st_line = end_line
-        atomCoor_Dict[i] = coor
-    fin.close()
+    # extract lattice constants and atom coordinates dictionary using POSCAR parser
+    latt_mat, latt_consts, _, _, atomCoor_Dict = parse_POSCAR(POSCAR)
 
-    ctrAtomCoor = atomCoor_Dict[ctrAtom_name][ctrAtom_index - 1].reshape((1, 3))  # coordinates of the central atom
-    length_a = np.linalg.norm(latt_para[0, :], 2)
-    length_b = np.linalg.norm(latt_para[1, :], 2)
-    length_c = np.linalg.norm(latt_para[2, :], 2)
+    # coordinates of the central atom
+    ctrAtomCoor = atomCoor_Dict[ctrAtom_name][ctrAtom_index - 1].reshape((1, 3))
+    length_a, length_b, length_c = latt_consts
     # calculate distances to the central atom
-    res = dict.fromkeys(atomCoor_Dict, [])
+    res = dict.fromkeys(atomCoor_Dict, {})
     for i in atomCoor_Dict.keys():
-        res[i] = []  # avoid name-binding problem!
+        res[i] = {}  # avoid name-binding problem!
         for coor in atomCoor_Dict[i]:
             currCoor = coor.reshape((1, 3))
-            # dist_base = atoms_dist(ctrAtomCoor, currCoor, latt_para) # distance within the simulation cell
+            # dist_base = atoms_dist(ctrAtomCoor, currCoor, latt_consts) # distance within the simulation cell
             index = np.where(np.all(atomCoor_Dict[i] == coor, axis=1))[0][0]
             # need to consider the periodic boundary condition
-            repetition_a = math.ceil(cutoff / length_a)  # upper boundary of number of adjacent cells to search in
+            repetition_a = math.ceil(cutoff / length_a)  # upper boundary of number of adjacent cells to search
             repetition_b = math.ceil(cutoff / length_b)
             repetition_c = math.ceil(cutoff / length_c)
             # usually a, b and c are not big values, so this nested loop does not take much time
@@ -90,8 +62,8 @@ def find_neighbors(ctrAtom, cutoff, POSCAR="POSCAR"):
                 for b in range(-repetition_b, repetition_b + 1):
                     for c in range(-repetition_c, repetition_c + 1):
                         dist_to = currCoor + np.array([[a, 0, 0]]) + np.array([[0, b, 0]]) + np.array([[0, 0, c]])
-                        if atoms_dist(ctrAtomCoor, dist_to, latt_para) <= cutoff:
-                            res[i].append({i + str(int(index + 1)): list(np.squeeze(dist_to))})
+                        if atoms_dist(ctrAtomCoor, dist_to, latt_mat) <= cutoff:
+                            res[i][i + str(int(index + 1))] = list(np.squeeze(dist_to))
     # remove the central atom from the dictionary
-    res[ctrAtom_name].remove({ctrAtom: list(np.squeeze(ctrAtomCoor))})
+    del res[ctrAtom_name][ctrAtom]
     return(res)
